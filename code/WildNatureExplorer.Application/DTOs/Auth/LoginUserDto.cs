820ad90f7e9845508d1bd58f2235d4c6f48ca784
Shyshou Maksim <149532479+SleeperKt@users.noreply.github.com>
2026-01-04 11:37:05 +0200

    Update LoginUserDto.cs

--- code/WildNatureExplorer.Application/DTOs/Auth/LoginUserDto.cs
+++ code/WildNatureExplorer.Application/DTOs/Auth/LoginUserDto.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations;
 namespace WildNatureExplorer.Application.DTOs.Auth;
 
 public record LoginUserDto(
-    
     [Required]
     [Description("User email, Example = alex2022@example.com")]
     string Email,
