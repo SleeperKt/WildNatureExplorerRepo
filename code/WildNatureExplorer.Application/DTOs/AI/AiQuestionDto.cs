e233ebceee621e30dd144e103087e359359d402a
Shyshou Maksim <149532479+SleeperKt@users.noreply.github.com>
2026-01-04 11:58:37 +0200

    Update AiQuestionDto.cs

--- code/WildNatureExplorer.Application/DTOs/AI/AiQuestionDto.cs
+++ code/WildNatureExplorer.Application/DTOs/AI/AiQuestionDto.cs
@@ -6,7 +6,7 @@ namespace WildNatureExplorer.Application.DTOs.AI
     public class AiQuestionDto
     {
         [Required]
-        [Description("Question for the Ai assistant, Example = What is the largest animal in the world?")]   
+        [Description("Question for the Ai assistant, Example = What is the largest animal in the world?")]
         public string? QuestionAboutNature { get; set; }
     }
 }
